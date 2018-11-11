0177bb28f58575cfe72eca8fbc535264296c766f
ZeroTheKing <44266197+ZeroTheKing@users.noreply.github.com>
2018-11-12 00:54:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,4 +353,20 @@ client.user.setGame(`Last Community`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
+client.on('ready',async () => {
+console.log("Starting..");
+let g = client.guilds.get("494250830363623454");
+let c = g.channels.get("508382831249063963");
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(!g.me.voiceChannel) c.join();
+}, 1);
+} else {
+console.log("Failed To Join:\n The Channel Type isn't \"text\"");
+}
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
